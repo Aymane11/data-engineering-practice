c19665660dda771318a17b46abd5afeb7b47ac34
enamya <aymaneboumaaza@gmail.com>
2023-11-11 22:09:11 +0100

    Add Ex2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -2,9 +2,15 @@ import requests
 import pandas
 
 
+URL = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+TARGET_DATE = "2022-02-07 14:03"
+
+
 def main():
-    # your code here
-    pass
+    scraped_df = pandas.read_html(URL)[0]
+    filename = scraped_df[scraped_df["Last modified"] == TARGET_DATE].iloc[0]["Name"]
+    df = pandas.read_csv(URL + filename)
+    print(df[df["HourlyDryBulbTemperature"] == df["HourlyDryBulbTemperature"].max()])
 
 
 if __name__ == "__main__":

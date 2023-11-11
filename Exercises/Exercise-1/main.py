076bc3c1baf35637b643960574903423670e7a56
enamya <aymaneboumaaza@gmail.com>
2023-11-11 21:55:36 +0100

    Refactor code

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,8 +1,10 @@
+from token import ASYNC
 import requests
 import zipfile
-import os
+from pathlib import Path
 import aiohttp
 from loguru import logger
+import asyncio
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -15,54 +17,58 @@ download_uris = [
 ]
 
 
-async def async_download_file(uri: str) -> str:
+async def download_file(uri: str, destination_dir: Path, async_mode: bool) -> str:
     filename = uri.split("/")[-1]
-    logger.info(f"Downloading {filename}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(uri) as resp:
-            with open(filename, "wb") as f:
-                f.write(await resp.read())
-    return filename
-
-
-def download_file(uri: str, destination_dir: str) -> str:
-    filename = uri.split("/")[-1]
-    filepath = os.path.join(destination_dir, filename)
+    filepath = destination_dir / filename
     logger.info(f"Downloading to {filepath}")
-    r = requests.get(uri)
-    with open(filepath, "wb") as f:
-        f.write(r.content)
+    if async_mode:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(uri) as resp:
+                with open(filepath, "wb") as f:
+                    f.write(await resp.read())
+    else:
+        r = requests.get(uri)
+        with open(filepath, "wb") as f:
+            f.write(r.content)
     return filepath
 
 
-def unzip_file(filename: str, destination_dir: str) -> None:
-    logger.info(f"Unzipping {filename}")
+def unzip_file(file: Path, destination_dir: Path) -> None:
+    logger.info(f"Unzipping {file}")
     try:
-        with zipfile.ZipFile(filename, "r") as zip_ref:
+        with zipfile.ZipFile(file, "r") as zip_ref:
             zip_ref.extractall(destination_dir)
     except zipfile.BadZipFile:
-        logger.error(f"File {filename} is not good zipfile")
+        logger.error(f"File {file} is not good zipfile")
 
 
-def remove_file(filename: str) -> None:
-    logger.info(f"Removing {filename}")
-    os.remove(filename)
+def remove_file(file: Path) -> None:
+    logger.info(f"Removing {file}")
+    file.unlink()
 
 
-def create_directory(directory: str) -> None:
+def create_directory(directory: str) -> Path:
     logger.info(f"Creating {directory}")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    return Path(directory)
 
 
-def main():
-    DOWN_DIR = "downloads"
-    create_directory(DOWN_DIR)
-    for uri in download_uris:
-        filename = download_file(uri, DOWN_DIR)
-        unzip_file(filename, DOWN_DIR)
-        remove_file(filename)
+async def process(uri: str, destination_dir: Path, async_mode: bool) -> None:
+    filename = await download_file(uri, destination_dir, async_mode)
+    unzip_file(filename, destination_dir)
+    remove_file(filename)
 
 
 if __name__ == "__main__":
-    main()
+    ASYNC_PROCESSING = True
+    download_dir = create_directory("downloads")
+    if ASYNC_PROCESSING:
+        logger.info("Starting async processing")
+        loop = asyncio.get_event_loop()
+        tasks = [process(uri, download_dir, True) for uri in download_uris]
+        loop.run_until_complete(asyncio.wait(tasks))
+        loop.close()
+    else:
+        logger.info("Starting sync processing")
+        for uri in download_uris:
+            asyncio.run(process(uri, download_dir, False))
